68c4a89660f9b99a2173a4aa8f164ce61ca3ce49
leonardo.goncalves <leogoncalves.contato@gmail.com>
2020-12-14 18:52:39 -0300

    refactor: atualizando packages

--- easy-store-web/src/environments/environment.prod.ts
+++ easy-store-web/src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+  urlBaseAPI: 'https://easy-store-dev.rj.r.appspot.com/',
+  urlBaseMockAPI: 'https://easy-store-dev.rj.r.appspot.com/',
+  firebaseConfig: {
+    apiKey: "AIzaSyCJzF59rPCvqgaRddU-WuPW8MVZChOadcM",
+    authDomain: "easy-store-dev.firebaseapp.com",
+    projectId: "easy-store-dev",
+    storageBucket: "easy-store-dev.appspot.com",
+    messagingSenderId: "982344674309",
+    appId: "1:982344674309:web:9d35173ff2281cf065406c",
+    measurementId: "G-VRJ4TWCN3N"
+  },
 };
